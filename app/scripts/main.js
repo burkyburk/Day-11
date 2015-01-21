4f8062c8b0381d1f64e28170e558cc551414c27b
Jacob Burkhart <jacobdouglasburkhart@gmail.com>
2015-01-21 17:44:15 -0600

    fixed animation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,35 +9,15 @@ r(min, 6*d.getMinutes());
 r(hour, 30*(d.getHours()%12) + d.getMinutes()/2);
 }, 1000);
 
-
-
-function changebackground()
-{
-window.setTimeout( "changebackground()", 1000); 
-
-var index = Math.round(Math.random() * 9);
-
-var ColorValue = "FFFFFF"; 
-
-if(index == 1)
-ColorValue = "FFCCCC"; 
-if(index == 2)
-ColorValue = "CCAFFF"; 
-if(index == 3)
-ColorValue = "A6BEFF"; 
-if(index == 4)
-ColorValue = "99FFFF"; 
-if(index == 5)
-ColorValue = "D5CCBB"; 
-if(index == 6)
-ColorValue = "99FF99"; 
-if(index == 7)
-ColorValue = "FFFF99"; 
-if(index == 8)
-ColorValue = "FFCC99"; 
-if(index == 9)
-ColorValue = "CCCCCC"; 
-
-document.getElementsByTagName("body")[0].style.backgroundColor = "#" + ColorValue;
-
-}
+var colours=['#ff0000','#00ff00','#0000ff','#acacac'];  // List of colors
+    var tempID=0;
+    var changeInterval=1000;    // Change interval in miliseconds
+    var objectID='#bgcolor';      // Object to change colours. 
+    
+    $(document).ready(function(){        
+        setInterval(function(){
+                $(objectID).animate({backgroundColor: colours[tempID]},200);
+                tempID=tempID+1
+                if (tempID>colours.length-1) tempID=0;
+            },changeInterval);
+    });
